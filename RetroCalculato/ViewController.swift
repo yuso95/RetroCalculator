f1595351111bcf09a7fb563436c0cf178428e106
Ousmane Y. Abdou <Yuso@Younoussas-MacBook-Pro.local>
2016-11-09 12:37:29 -0500

    Add SFX

--- RetroCalculato/ViewController.swift
+++ RetroCalculato/ViewController.swift
@@ -7,15 +7,42 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var btnSound: AVAudioPlayer!
+    
+    @IBAction func BTNPressed(_ sender: UIButton) {
+        
+        playSound()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let path = Bundle.main.path(forResource: "btn", ofType: "wav")
+        let soundURL = URL(fileURLWithPath: path!)
         
+        do {
+            
+            try btnSound = AVAudioPlayer(contentsOf: soundURL)
+            
+            btnSound.prepareToPlay()
+        } catch let err as NSError {
+            
+            print(err.debugDescription)
+        }
     }
-
-
+    
+    func playSound() {
+        
+        if btnSound.isPlaying {
+            
+            btnSound.stop()
+        }
+        
+        btnSound.play()
+    }
 }
 
